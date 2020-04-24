f0e93f39f36ec36e47e1b4fa45420c4b79ca9067
Sławomir Rodak <slawomir.rodak@schibsted.pl>
2020-04-24 13:12:15 +0200

    remove `new RTCSessionDescription` constructor

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,36 +3,36 @@ let getCalled = false;
 
 const existingCalls = [];
 
-const { RTCPeerConnection, RTCSessionDescription } = window;
+const {RTCPeerConnection, RTCSessionDescription} = window;
 
 const peerConnection = new RTCPeerConnection();
 
 function unselectUsersFromList() {
   const alreadySelectedUser = document.querySelectorAll(
-    ".active-user.active-user--selected"
+      '.active-user.active-user--selected',
   );
 
   alreadySelectedUser.forEach(el => {
-    el.setAttribute("class", "active-user");
+    el.setAttribute('class', 'active-user');
   });
 }
 
 function createUserItemContainer(socketId) {
-  const userContainerEl = document.createElement("div");
+  const userContainerEl = document.createElement('div');
 
-  const usernameEl = document.createElement("p");
+  const usernameEl = document.createElement('p');
 
-  userContainerEl.setAttribute("class", "active-user");
-  userContainerEl.setAttribute("id", socketId);
-  usernameEl.setAttribute("class", "username");
+  userContainerEl.setAttribute('class', 'active-user');
+  userContainerEl.setAttribute('id', socketId);
+  usernameEl.setAttribute('class', 'username');
   usernameEl.innerHTML = `Socket: ${socketId}`;
 
   userContainerEl.appendChild(usernameEl);
 
-  userContainerEl.addEventListener("click", () => {
+  userContainerEl.addEventListener('click', () => {
     unselectUsersFromList();
-    userContainerEl.setAttribute("class", "active-user active-user--selected");
-    const talkingWithInfo = document.getElementById("talking-with-info");
+    userContainerEl.setAttribute('class', 'active-user active-user--selected');
+    const talkingWithInfo = document.getElementById('talking-with-info');
     talkingWithInfo.innerHTML = `Talking with: "Socket: ${socketId}"`;
     callUser(socketId);
   });
@@ -42,16 +42,16 @@ function createUserItemContainer(socketId) {
 
 async function callUser(socketId) {
   const offer = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+  await peerConnection.setLocalDescription((offer));
 
-  socket.emit("call-user", {
+  socket.emit('call-user', {
     offer,
-    to: socketId
+    to: socketId,
   });
 }
 
 function updateUserList(socketIds) {
-  const activeUserContainer = document.getElementById("active-user-container");
+  const activeUserContainer = document.getElementById('active-user-container');
 
   socketIds.forEach(socketId => {
     const alreadyExistingUser = document.getElementById(socketId);
@@ -63,13 +63,13 @@ function updateUserList(socketIds) {
   });
 }
 
-const socket = io.connect("/");
+const socket = io.connect('/');
 
-socket.on("update-user-list", ({ users }) => {
+socket.on('update-user-list', ({users}) => {
   updateUserList(users);
 });
 
-socket.on("remove-user", ({ socketId }) => {
+socket.on('remove-user', ({socketId}) => {
   const elToRemove = document.getElementById(socketId);
 
   if (elToRemove) {
@@ -77,17 +77,17 @@ socket.on("remove-user", ({ socketId }) => {
   }
 });
 
-socket.on("call-made", async data => {
+socket.on('call-made', async data => {
   if (getCalled) {
     const confirmed = window.confirm(
-      `User "Socket: ${data.socket}" wants to call you. Do accept this call?`
+        `User "Socket: ${data.socket}" wants to call you. Do accept this call?`,
     );
 
-    console.log(confirmed)
+    console.log(confirmed);
 
     if (!confirmed) {
-      socket.emit("reject-call", {
-        from: data.socket
+      socket.emit('reject-call', {
+        from: data.socket,
       });
 
       return;
@@ -95,52 +95,52 @@ socket.on("call-made", async data => {
   }
 
   await peerConnection.setRemoteDescription(
-    new RTCSessionDescription(data.offer)
+      (data.offer),
   );
   const answer = await peerConnection.createAnswer();
-  await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+  await peerConnection.setLocalDescription((answer));
 
-  socket.emit("make-answer", {
+  socket.emit('make-answer', {
     answer,
-    to: data.socket
+    to: data.socket,
   });
   getCalled = true;
 });
 
-socket.on("answer-made", async data => {
+socket.on('answer-made', async data => {
   await peerConnection.setRemoteDescription(
-    new RTCSessionDescription(data.answer)
+      (data.answer),
   );
 
   if (!isAlreadyCalling) {
-    callUser(data.socket);
+    await callUser(data.socket);
     isAlreadyCalling = true;
   }
 });
 
-socket.on("call-rejected", data => {
+socket.on('call-rejected', data => {
   alert(`User: "Socket: ${data.socket}" rejected your call.`);
   unselectUsersFromList();
 });
 
-peerConnection.ontrack = function({ streams: [stream] }) {
-  const remoteVideo = document.getElementById("remote-video");
+peerConnection.ontrack = function({streams: [stream]}) {
+  const remoteVideo = document.getElementById('remote-video');
   if (remoteVideo) {
     remoteVideo.srcObject = stream;
   }
 };
 
 navigator.getUserMedia(
-  { video: true, audio: true },
-  stream => {
-    const localVideo = document.getElementById("local-video");
-    if (localVideo) {
-      localVideo.srcObject = stream;
-    }
+    {video: true, audio: true},
+    stream => {
+      const localVideo = document.getElementById('local-video');
+      if (localVideo) {
+        localVideo.srcObject = stream;
+      }
 
-    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-  },
-  error => {
-    console.warn(error.message);
-  }
+      stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+    },
+    error => {
+      console.warn(error.message);
+    },
 );
